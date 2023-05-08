a9100f34d8ac6a39db5f4045bb5577e6c948636e
Joeny Vo <jvo1563@gmail.com>
2023-05-07 19:46:01 -0700

    updated error handling

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ app.get("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
   if (!body.content) {
@@ -48,9 +48,12 @@ app.post("/api/notes", (request, response) => {
     content: body.content,
     important: body.important || false,
   });
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/notes/:id", (request, response, next) => {
@@ -62,12 +65,12 @@ app.delete("/api/notes/:id", (request, response, next) => {
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-  Note.findByIdAndUpdate(request.params.id, note, { new: true }) // without new: true the updatedNote would be the old original note
+  const { content, important } = request.body;
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  ) // without new: true the updatedNote would be the old original note
     .then((updatedNote) => {
       response.json(updatedNote);
     })
@@ -83,6 +86,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
